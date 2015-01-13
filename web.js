016154a26773a471b90ed237bfc0711fe4bac475
Kyle Bowerman <kyle.bowerman@appirio.com>
2015-01-13 12:05:23 -0600

    fixed mongo limit by sorting desc by eopoch, added subway mean time

--- web.js
+++ web.js
@@ -1,16 +1,23 @@
 var express = require('express');
 var  app = express();
 var request = require('request');
-var port = process.env.PORT || 3000
+var moment = require('moment');
+var port = process.env.PORT || 3000;
 
 
+console.log('the time is ' + moment());
+console.log('the time 24 hours ago was ' + moment().subtract(24,'hours'));
+var ago24hours =  moment().subtract(12,'hours');
 
 
 
 var apikey = process.env.MONGO_APIKEY;
-var mongoQuery = encodeURIComponent('{"version":2}');
+//var mongoQuery = encodeURIComponent('{"version":2}');
+var mongoQuery = "";
 var mongoFields = encodeURIComponent('{"pin":1,"device":1,"value":1,"time":1,"epochtime":1,"_id":0}');
-var url = "https://api.mongolab.com/api/1/databases/arduino/collections/kubli?q="+mongoQuery+"&f="+mongoFields+"&apiKey="+apikey;
+var mongobase = 'https://api.mongolab.com/api/1/databases/arduino/collections/kubli'
+var mongosort = encodeURIComponent('{"epochtime":-1}');
+//var url = "https://api.mongolab.com/api/1/databases/arduino/collections/kubli?q="+mongoQuery+"&f="+mongoFields+"&apiKey="+apikey;
 
 
 var options = {
@@ -27,18 +34,20 @@ var options = {
 
 var getData = function(req,res, next) {
   mongoQuery = encodeURIComponent('{"version":2}');
-  url = "https://api.mongolab.com/api/1/databases/arduino/collections/kubli?q="+mongoQuery+"&f="+mongoFields+"&apiKey="+apikey;
+  url = mongobase+"?q="+mongoQuery+"&f="+mongoFields+"&s="+mongosort+"&l=1000&apiKey="+apikey;
+  console.log('the aurl requested is ' + decodeURIComponent(url));
+
   request(url, function (error,response,body){
     if (!error && response.statusCode == 200) {
-      //console.log(body);
       res.send(body);
     }
   });
-};
+  };
+
 
 var getA0 = function(req,res, next) {
   mongoQuery = encodeURIComponent('{"version":2, "pin": "A0"}');
-  url = "https://api.mongolab.com/api/1/databases/arduino/collections/kubli?q="+mongoQuery+"&f="+mongoFields+"&apiKey="+apikey;
+  url = mongobase+"?q="+mongoQuery+"&f="+mongoFields+"&s="+mongosort+"&l=1000&apiKey="+apikey;
   request(url, function (error,response,body){
     if (!error && response.statusCode == 200) {
       //console.log(body);
@@ -49,7 +58,7 @@ var getA0 = function(req,res, next) {
 
 var getA1 = function(req,res, next) {
   mongoQuery = encodeURIComponent('{"version":2, "pin": "A1"}');
-  url = "https://api.mongolab.com/api/1/databases/arduino/collections/kubli?q="+mongoQuery+"&f="+mongoFields+"&apiKey="+apikey;
+  url = mongobase+"?q="+mongoQuery+"&f="+mongoFields+"&s="+mongosort+"&l=1000&apiKey="+apikey;
   request(url, function (error,response,body){
     if (!error && response.statusCode == 200) {
       //console.log(body);
@@ -57,10 +66,6 @@ var getA1 = function(req,res, next) {
     }
   });
 };
-
-
-
-
 
 
 
