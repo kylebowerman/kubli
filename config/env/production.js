f3e76c41463c8d0a2d23d834d795148f51ce772b
Kyle Bowerman <kyle.bowerman@appirio.com>
2015-01-14 14:51:45 -0600

    x

--- config/env/production.js
+++ config/env/production.js
@@ -4,7 +4,7 @@ module.exports = {
   db:  process.env.MONGOHQ_URL || 'mongodb://' + (process.env.DB_PORT_27017_TCP_ADDR || 'localhost') + '/mean-prod',
   /**
    * Database options that will be passed directly to mongoose.connect
-   * Below are some examples.
+   * Below are some examples. 
    * See http://mongodb.github.io/node-mongodb-native/driver-articles/mongoclient.html#mongoclient-connect-options
    * and http://mongoosejs.com/docs/connections.html for more information
    */
