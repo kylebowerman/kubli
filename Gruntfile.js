f4b3c4100bd284e1b3ebae2f805c073804cffdf9
Kyle Bowerman <kyle.bowerman@appirio.com>
2015-01-14 23:50:09 -0600

    heroku

--- Gruntfile.js
+++ Gruntfile.js
@@ -137,5 +137,5 @@ module.exports = function(grunt) {
   // For Heroku users only.
   // Docs: https://github.com/linnovate/mean/wiki/Deploying-on-Heroku
   grunt.registerTask('heroku:production', ['cssmin', 'uglify']);
-  grunt.registerTask('heroku:dev', ['cssmin', 'uglify']);
+
 };
