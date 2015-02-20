0e932da31814761becf8bfee5ef12bbfee263dc8
Kyle Bowerman <kyle.bowerman@appirio.com>
2015-02-20 14:53:04 -0600

    added boostrap to asset.json to be minified

--- config/env/production.js
+++ config/env/production.js
@@ -1,8 +1,7 @@
 'use strict';
 
 module.exports = {
-  //db:  process.env.MONGOHQ_URL || 'mongodb://' + (process.env.DB_PORT_27017_TCP_ADDR || 'localhost') + '/mean-prod',
-  db:  process.env.MONGOHQ_URL,
+  db: process.env.MONGOHQ_URL,
   /**
    * Database options that will be passed directly to mongoose.connect
    * Below are some examples.
